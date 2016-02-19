93dde3da6810b927d2219a8b729fdf4acaa01efb
john mgbako <john mgbako>
2016-02-19 14:39:07 +0100

    corrected a typo case sensitive in examhall

--- resources/views/exams/index.blade.php
+++ resources/views/exams/index.blade.php
@@ -31,7 +31,7 @@
                        </div>
                          
                         @foreach($subjects as $subject)
-                        @if(Scholr\Subjectquestionstatus::canwrite($classe_id, $subject->id, 1))
+                        @if(Scholr\SubjectQuestionstatus::canwrite($classe_id, $subject->id, 1))
 
                           @if( Scholr\Grade::where(['student_id'=> $records->id, 'classe_id'=> $classe_id, 'subject_id'=> $subject->id])->first() )  
                             <div class="box box-warning box-solid" style="opacity: .5;">
