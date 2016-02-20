090d6d397abe58fafb1f6b83dee131ee9fde51df
Thankgod Ossaija <Thankgod Ossaija>
2016-02-20 07:52:09 +0100

    added more text to password reset view

--- resources/views/emails/password.blade.php
+++ resources/views/emails/password.blade.php
@@ -1,1 +1,3 @@
-Click here to reset your password: {{ url('password/reset/'.$token) }}
+<h3>Password Reset Request</h3>
+<p>A password reset was request with this email, if you did request it pleace click on the link below to reset your password or ignore it if you did not.</p>
+<a href="{{ url('password/reset/'.$token) }}">Click here to reset your password</a>
