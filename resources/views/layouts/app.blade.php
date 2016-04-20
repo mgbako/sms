cb44bd4ed68135dc17fe4ce0edbbb98185bb788b
Ossaija ThankGod <codebugsolved@gmail.com>
2016-04-20 03:52:42 +0100

    clean up

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	@if (School::first()->name)
+	@if (School::first())
 		<title>{{ucfirst(School::first()->name)}}</title>
 	@else
 		<title>AddTen</title>
