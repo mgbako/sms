4991ded3568db7da97fcc1b6c9fd461d27b451ac
Ossaija ThankGod <codebugsolved@gmail.com>
2016-04-20 03:50:43 +0100

    clean up

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,8 +4,12 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-
-	<title>{{ucfirst(School::first()->name)}}</title>
+	@if (School::first()->name)
+		<title>{{ucfirst(School::first()->name)}}</title>
+	@else
+		<title>AddTen</title>
+	@endif
+	
 
 	<link href="/css/app.css" rel="stylesheet">
 	<link rel="stylesheet" type="text/css" href="{{ asset('/css/bootstrap.min.css') }}">
